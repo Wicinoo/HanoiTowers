b20c40ae6948f9277b2f641fc54b55d69840a877
Wicinoo <the.wicinoo@gmail.com>
2014-12-06 14:32:15 +0100

    Loaded Unity Project

---
+++ Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	private StoneControl selectedStone;
+
+	public Vector3[] PolePositions;
+
+
+	void Start()
+	{
+
+	}
+
+	void Update () 
+	{
+		if (selectedStone == null) return;
+
+		if (Input.GetKeyUp(KeyCode.W)) selectedStone.PickUp();
+		if (Input.GetKeyUp(KeyCode.S)) selectedStone.Drop();
+		if (Input.GetKeyUp(KeyCode.D)) selectedStone.MoveRight();
+		if (Input.GetKeyUp(KeyCode.A)) selectedStone.MoveLeft();	
+	}
+
+	public void SelectStone(StoneControl newSelection)
+	{
+		selectedStone = newSelection;
+	}
+
+	public float GetPolePositionX(int pole)
+	{
+		return PolePositions[pole].x;
+	}
+}
---
+++ Assets/StoneControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneControl : MonoBehaviour 
+{
+	public int Size;
+
+	private GameController gc;
+	
+	private bool isMoving = false;
+	private bool isUp = false;
+	private int currentPole=1;
+
+	void Start()
+	{
+		gc = GameObject.Find("GameController").GetComponent<GameController>();
+		if (gc == null) throw new MissingComponentException("Game Controller not found!");
+	}
+
+	void OnMouseOver()
+	{
+		if (Input.GetKeyUp(KeyCode.Mouse0)) 
+		{
+			gc.SelectStone(this);
+			Debug.Log(this.transform.name);
+		}
+	}
+
+
+	public void PickUp()
+	{
+		isUp = true;
+	}
+
+	public void Drop()
+	{
+		isUp = false;
+	}
+
+	public void MoveLeft()
+	{
+		if (currentPole <= 0) return;
+		currentPole--;
+		UpdatePosition();
+	}
+
+	public void MoveRight()
+	{
+		if (currentPole >= 2) return;
+		currentPole++;
+		UpdatePosition();
+	}
+
+	private void UpdatePosition()
+	{
+		this.transform.Translate(gc.GetPolePositionX(currentPole),0,0);
+	}
+}
